55264eb6be0ab98d9327844168d733cb6d5f6590
Joshua <33937944+elementzprojects@users.noreply.github.com>
2020-08-27 20:08:04 -0400

    Update drawRect.js

--- drawRect.js
+++ drawRect.js
@@ -16,45 +16,8 @@ for (let i = 0; i < labels.length; i++){
   `
   selectLabel(labels[i]);
 }
-document.getElementById("btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  const filee = document.querySelector("input[type=file]").files;
-  let objects = dataPrep(filee.length);
-  let oldX = 0;
-  let oldY = 0;
-  let x = 0;
-  let y = 0;
-  let width = 0;
-  let height = 0;
-  displayImage(imageIndex, filee);
 
-  let isMouseDown = false;
-  output.addEventListener("mousedown", mouseDown);
-  // output.addEventListener("mousemove", draw);
-  output.addEventListener("mouseup", mouseUp);
-  function mouseDown(e) {
-    oldX = e.clientX - boundingBoxes.left;
-    oldY = e.clientY - boundingBoxes.top;
-    objects[parseInt(imageIndex)].objectExists = 1;
-    isMouseDown = true;
-    output.onmousemove = (e) => {
-      x = e.clientX - boundingBoxes.left;
-      y = e.clientY - boundingBoxes.top;
-      if (isMouseDown) {
-        width = x - oldX;
-        height = y - oldY;
-        context.fillStyle = "#000";
-        context.clearRect(0, 0, output.width, output.height);
-        context.strokeRect(oldX, oldY, width, height);
-        modifyJSON(objects, parseInt(imageIndex), oldX, oldY, width, height, label);
 
-      }
-    };
-  }
-  function mouseUp(e) {
-    isMouseDown = false;
-  }
-});
 function dataPrep(length) {
   const object = {
     objectExists: 0,
@@ -94,29 +57,29 @@ function displayImage(imageIndex, file) {
     // output.style.height = "250px";
   };
 
-  if (file[parseInt(imageIndex)]) reader.readAsDataURL(file[parseInt(imageIndex)]);
+  if (file[imageIndex]) reader.readAsDataURL(file[imageIndex]);
   else output.src = "";
 }
 previousImage.addEventListener("click", (e) => {
   if (imageIndex >= 1) {
     imageIndex--;
-    displayImage(parseInt(imageIndex), document.querySelector("input[type=file]").files);
+    displayImage(imageIndex, document.querySelector("input[type=file]").files);
   }
 });
 nextImage.addEventListener("click", (e) => {
   if (imageIndex < imageLen) {
     imageIndex++;
     context.clearRect(0, 0, output.width, output.height);
-    displayImage(parseInt(imageIndex), document.querySelector("input[type=file]").files);
+    displayImage(imageIndex, document.querySelector("input[type=file]").files);
   }
 });
 
 function modifyJSON(obj, imageIndex, x, y, width, height, label) {
-  obj[parseInt(imageIndex)]["midPointX"] = (x + width / 2) / output.width;
-  obj[parseInt(imageIndex)]["midPointY"] = (y + height / 2) / output.height;
-  obj[parseInt(imageIndex)]["width"] = Math.abs(width / output.width);
-  obj[parseInt(imageIndex)]["height"] = Math.abs(height / output.height);
-  obj[parseInt(imageIndex)][label] = 1;
+  obj[imageIndex]["midPointX"] = (x + width / 2) / output.width;
+  obj[imageIndex]["midPointY"] = (y + height / 2) / output.height;
+  obj[imageIndex]["width"] = Math.abs(width / output.width);
+  obj[imageIndex]["height"] = Math.abs(height / output.height);
+  obj[imageIndex][label] = 1;
   textarea.innerHTML = JSON.stringify(obj);
 }
 function selectLabel(id){
@@ -137,3 +100,42 @@ function copy(){
   textarea.select();
   document.execCommand("copy");
 }
+
+document.getElementById("btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  const filee = document.querySelector("input[type=file]").files;
+  let objects = dataPrep(filee.length);
+  let oldX = 0;
+  let oldY = 0;
+  let x = 0;
+  let y = 0;
+  let width = (height = 0);
+  displayImage(imageIndex, filee);
+
+  let isMouseDown = false;
+  function mouseDown(e) {
+    oldX = e.clientX - boundingBoxes.left;
+    oldY = e.clientY - boundingBoxes.top;
+    objects[imageIndex].objectExists = 1;
+    isMouseDown = true;
+    output.onmousemove = (e) => {
+      x = e.clientX - boundingBoxes.left;
+      y = e.clientY - boundingBoxes.top;
+      if (isMouseDown) {
+        width = x - oldX;
+        height = y - oldY;
+        context.fillStyle = "#000";
+        context.clearRect(0, 0, output.width, output.height);
+        context.strokeRect(oldX, oldY, width, height);
+        modifyJSON(objects, imageIndex, oldX, oldY, width, height, label);
+
+      }
+    };
+  }
+  function mouseUp(e) {
+    isMouseDown = false;
+  }
+  output.addEventListener("mousedown", mouseDown);
+  // output.addEventListener("mousemove", draw);
+  output.addEventListener("mouseup", mouseUp);
+});
